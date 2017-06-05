70942e3f8df0a13920e6254a228d3891976d95ac
Zhi Liu <james.zhi.liu@gmail.com>
2017-06-05 00:17:09 -0400

    removed spi pullups

--- Inc/main.h
+++ Inc/main.h
@@ -94,8 +94,6 @@
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
 #define TCK_GPIO_Port GPIOA
-#define SWO_Pin GPIO_PIN_3
-#define SWO_GPIO_Port GPIOB
 #define MCP2_CS_Pin GPIO_PIN_8
 #define MCP2_CS_GPIO_Port GPIOB
 
--- Src/main.c
+++ Src/main.c
@@ -617,10 +617,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB0 PB13 PB14 PB4 
-                           PB7 PB9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_4 
-                          |GPIO_PIN_7|GPIO_PIN_9;
+  /*Configure GPIO pins : PB0 PB13 PB14 PB3 
+                           PB4 PB7 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_3 
+                          |GPIO_PIN_4|GPIO_PIN_7|GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
